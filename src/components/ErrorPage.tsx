ddf2513e6fc4a0ab9ed36d5b24995a66b8d2bd87
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2023-12-08 15:07:37 +0100

    P

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,4 +1,3 @@
-// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5p4NgHTV6KxfcHV0VsOx47SnDACApaFxn1iXVpYgVsxmDkUV5bN-WFmWXQ-QmCmkSsTA&usqp=CAU
 import { useRouteError } from "react-router-dom";
 const ErrorPage = () => {
     const error: any = useRouteError();
@@ -7,7 +6,7 @@ const ErrorPage = () => {
     if (error.data == 404) {
         return (
             <div className="flex p-12 h-screen bg-slate-50 flex-col items-center">
-                <img src="/public/404.png" className="h-[40%]" alt="404 Image" />
+                <img src="../../public/404.png" className="h-[40%]" alt="404 Image" />
                 <div className="mt-2">
                     <h1 className="text-center text-2xl ">
                         <span className="text-red-400">oops!</span> Page not
